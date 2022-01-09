040ccad9a6940bd46540386ad4b935aca6978d4c
barinova-alevel <you@example.com>
2022-01-09 16:16:28 +0200

    initial commit

---
+++ Catalog/Catalog.Host/Controllers/CatalogController.cs
@@ -0,0 +1,24 @@
+﻿using Catalog.Host.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Host.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CatalogController : ControllerBase
+    {
+        private static readonly Product[] products = new[]
+        {
+            new Product() { Id = 1, Name = "OOOOOOOppo A53s", Price = 7990, Avatar = "https://cdn.comfy.ua/media/cms/cat_2/smartfony-xiaomi.png"},
+            new Product() { Id = 2, Name = "Apple iPhone 13 Pro", Price = 38499, Avatar = "https://cdn.comfy.ua/media/cms/cat_2/smartfony-apple.png"},
+            new Product() { Id = 3, Name = "Apple iPhone 11 64Gb Black", Price = 17999, Avatar = "https://cdn.comfy.ua/media/x/img/file_2.png"},
+            new Product() { Id = 4, Name = "Oppo A53s", Price = 7990, Avatar = "https://cdn.comfy.ua/media/cms/cat_2/smartfony-xiaomi.png"},
+        };
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return products;
+        }
+    }
+}
---
+++ Catalog/Catalog.Host/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.Host.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+        public string? Avatar { get; set; }
+    }
+}
